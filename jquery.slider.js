90d9c06cde7ad28ee56cf10d9b7d01b53debce63
Kurt Kellens <kurt.kellens@pie.nl>
2019-12-16 10:06:58 +0100

    code cleanup

--- jquery.slider.js
+++ jquery.slider.js
@@ -1,42 +1,38 @@
-(function($){
-			$(function()
-			{
-				var isMouseDown = false,
-					$panelOne = $(".panel.one"), 
-					$panelTwo = $(".panel.two"), 
-					$panelContainer = $panelOne.parent(), 
-					getParentWidth = function(){ return $panelContainer.width();}, 
-					mouseMoveHandler = function(e){
-						if(!isMouseDown)
-							return;
+(function($) {
+  $(function() {
+    var isMouseDown = false,
+      $panelOne = $(".panel.one"),
+      $panelTwo = $(".panel.two"),
+      $panelContainer = $panelOne.parent(),
+      getParentWidth = function() {
+        return $panelContainer.width();
+      },
+      mouseMoveHandler = function(e) {
+        if (!isMouseDown) return;
 
-						var w = (e.clientX / getParentWidth()) * 100;
-						
-						// don't allow a value that's smaller than zero;
-						w = w < 0 ? 0 : w;
+        var width = (e.clientX / getParentWidth()) * 100;
 
-						// apply size to panel 1
-						$panelOne.css({width:w + "%"});
-						
-						// apply size to panel 2
-						$panelTwo.css({width:(100-w) + "%"});			
-				};
-				
-				// mouseDown event				
-				$(".slider").on("mousedown", function()
-				{
-					// only bind a the mouseMove handler on the first cycle
-					!isMouseDown && $panelContainer.on("mousemove",mouseMoveHandler);
-					isMouseDown = true;
-				});
-					
-				$(window).on("mouseup", function(){
-					isMouseDown = false;
-					// detach then mouseMove handler
-					$panelContainer.off("mousemove");
-				});
-					
-					
-				//$panelContainer.on("mousemove",mouseMoveHandler);
-			})
-		})(jQuery)
+        // don't allow a value that's smaller than zero;
+        width = width < 0 ? 0 : width;
+
+        // apply size to panel 1
+        $panelOne.css({ width: width + "%" });
+
+        // apply size to panel 2
+        $panelTwo.css({ width: 100 - width + "%" });
+      };
+
+    // mouseDown event
+    $(".slider").on("mousedown", function() {
+      // only bind a the mouseMove handler on the first cycle
+      !isMouseDown && $panelContainer.on("mousemove", mouseMoveHandler);
+      isMouseDown = true;
+    });
+
+    $(window).on("mouseup", function() {
+      isMouseDown = false;
+      // detach then mouseMove handler
+      $panelContainer.off("mousemove");
+    });
+  });
+})(jQuery);
